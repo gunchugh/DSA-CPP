bcd4f5a08725606991c889b40d34d0cde6087f55
Gun Chugh <gunnchugh58@gmail.com>
2026-08-30 16:50:39 +0530

    Added more

---
+++ input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "Enter the value of x and y:";
+    cin >> x >> y ;
+    cout << "Value of x and y:"<< x << " and " << y << endl;
+return 0;
+}
